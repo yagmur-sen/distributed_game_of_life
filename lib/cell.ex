8692f5b186c62048f71207ed8b85bfb361ff405d
yagmur-sen <yagmur.sen@unifr.ch>
2025-12-12 12:46:18 +0100

    refactor Cell module to use GenServer calls for setting neighbors and handling ticks

--- lib/cell.ex
+++ lib/cell.ex
@@ -3,7 +3,10 @@ defmodule Cell do
 
     def start_link(opts \\ []) do
         initial_state = Keyword.get(opts, :state, :dead)
-        GenServer.start_link(__MODULE__, %{state: initial_state, neighbors: []})
+        GenServer.start_link(__MODULE__, %{
+            state: initial_state, 
+            next_state: nil,
+            neighbors: []})
     end
 
     def get_state(pid) do
@@ -11,11 +14,18 @@ defmodule Cell do
     end
 
     def set_neighbors(pid, neighbor_pids) when is_list(neighbor_pids) do
-        GenServer.cast(pid, {:set_neighbors, neighbor_pids})
+        GenServer.call(pid, {:set_neighbors, neighbor_pids})
+    end
+
+    def tick_start(pid) do
+        GenServer.call(pid, :tick_start)
+    end
+
+    def tick_end(pid) do
+        GenServer.call(pid, :tick_end)
     end
 
     #Callbacks
-
     @impl true
     def init(state_map) do
         {:ok, state_map}
@@ -27,8 +37,37 @@ defmodule Cell do
     end
 
     @impl true
-    def handle_cast({:set_neighbors, neighbor_pids}, state) do
-        {:noreply, %{state | neighbors: neighbor_pids}}
+    def handle_call(:tick_start, _from, state) do
+        # Get neighbor states
+        alive_count = count_alive_neighbors(state.neighbors)
+
+        # Apply game rules
+        next_state = case {state.state, alive_count} do
+            {:alive, 2} -> :alive
+            {:alive, 3} -> :alive
+            {:dead, 3} -> :alive
+            _ -> :dead
+        end
+
+        {:reply, :ok, %{state | next_state: next_state}}
+    end
+
+    @impl true
+    def handle_call(:tick_end, _from, state) do
+        {:reply, :ok, %{state | state: state.next_state}}
+    end
+        
+
+    @impl true
+    def handle_call({:set_neighbors, neighbor_pids}, _from, state) do
+        {:reply, :ok, %{state | neighbors: neighbor_pids}}
+    end
+
+    # Helper function
+    defp count_alive_neighbors(neighbor_pids) do
+        neighbor_pids
+        |> Enum.map(&get_state/1)
+        |> Enum.count(fn cell_state -> cell_state == :alive end)
     end
 
 end
